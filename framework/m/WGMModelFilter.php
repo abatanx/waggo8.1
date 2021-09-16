4b6f3476ced78ae81fe2f9cc5d2818d61a423b12
Takao Kobayashi <abatanx@gmail.com>
2021-09-16 17:45:38 +0900

    Working

--- framework/m/WGMModelFilter.php
+++ framework/m/WGMModelFilter.php
@@ -7,9 +7,29 @@
 
 class WGMModelFilter
 {
-	public function __construct(){}
-	public function input($v) { return $v; }
-	public function output($v) { return $v; }
-	public function modelToView($obj,$v) { $obj->setValue($v); }
-	public function viewToModel($obj) { return $obj->getValue(); }
+	public function __construct()
+	{
+	}
+
+	public function input( mixed $value ): mixed
+	{
+		return $value;
+	}
+
+	public function output( mixed $value ): mixed
+	{
+		return $value;
+	}
+
+	public function modelToView( WGV8Object $view, mixed $v ): self
+	{
+		$view->setValue( $v );
+
+		return $this;
+	}
+
+	public function viewToModel( WGV8Object $view ): mixed
+	{
+		return $view->getValue();
+	}
 }
