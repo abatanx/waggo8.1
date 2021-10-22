c632a0daab6ddd5f75404c4588595c2c51bf6634
Takao Kobayashi <abatanx@gmail.com>
2021-10-22 09:28:54 +0900

    Add: Sample gauntlet pattern (tel number)

--- ut/tests/gauntlet/FrameworkGauntletWGGChainTest.php
+++ ut/tests/gauntlet/FrameworkGauntletWGGChainTest.php
@@ -10,6 +10,12 @@ use PHPUnit\Framework\TestCase;
 require_once __DIR__ . '/local-common.php';
 
 require_once __DIR__ . '/../../../framework/gauntlet/WGG.php';
+require_once __DIR__ . '/../../../framework/gauntlet/WGGFilterTrim.php';
+require_once __DIR__ . '/../../../framework/gauntlet/WGGFilterAnk.php';
+require_once __DIR__ . '/../../../framework/gauntlet/WGGString.php';
+require_once __DIR__ . '/../../../framework/gauntlet/WGGReg.php';
+require_once __DIR__ . '/../../../framework/gauntlet/WGGEmpty.php';
+require_once __DIR__ . '/../../../framework/gauntlet/WGGThru.php';
 
 class WGGTest extends WGG
 {
@@ -82,4 +88,47 @@ class FrameworkGauntletWGGChainTest extends TestCase
 		$root->valid($valid)->invalid($invalid)->check($r);
 		$this->assertEquals('2', $r);
 	}
+
+	static function sample_telephone_gauntlet( $allow_empty = true )
+	{
+		$main = WGGFilterTrim::_( true )->add( WGGFilterAnk::_()->add( WGGString::_( 1, 100 )
+																				->add( WGGReg::_( '/^[0-9]+$/' ) ) ) );
+		if ( $allow_empty )
+		{
+			return WGGEmpty::_()->add( WGGThru::_(), $main );
+		}
+		return $main;
+	}
+
+	public function test_wgg_chain_sample_test()
+	{
+		$tel = '08012345678';
+		$this->assertFalse(self::sample_telephone_gauntlet(true)->check($tel)->hasError());
+		$this->assertSame('08012345678', $tel);
+
+		$tel = '';
+		$this->assertFalse(self::sample_telephone_gauntlet(true)->check($tel)->hasError());
+		$this->assertSame('', $tel);
+
+		$tel = '08012345678';
+		$this->assertFalse(self::sample_telephone_gauntlet(false)->check($tel)->hasError());
+		$this->assertSame('08012345678', $tel);
+
+		$tel = '';
+		$this->assertTrue(self::sample_telephone_gauntlet(false)->check($tel)->hasError());
+		$this->assertSame('', $tel);
+
+		$tel = '０９０１２３４５６７８';
+		$this->assertFalse(self::sample_telephone_gauntlet(true)->check($tel)->hasError());
+		$this->assertSame('09012345678', $tel);
+
+		$tel = '　０９０１２３４５６７８　';
+		$this->assertFalse(self::sample_telephone_gauntlet(false)->check($tel)->hasError());
+		$this->assertSame('09012345678', $tel);
+
+		$tel = '　０９０　１２３４　５６７８　';
+		$this->assertTrue(self::sample_telephone_gauntlet(false)->check($tel)->hasError());
+		$this->assertSame('090 1234 5678', $tel);
+	}
 }
+
